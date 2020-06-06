04121a03a5743193ea67889ae18217304b261e58
Hiroyuki Tanaka <pillyshi21@gmail.com>
2020-06-06 13:48:31 +0900

    Refactoring

--- recommender_system_tutorial/types.py
+++ recommender_system_tutorial/types.py
@@ -1,19 +1,9 @@
-from typing import List
-
-
-class Context(object):
-    
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-
 class Feedback(object):
     pass
 
 
 class ImplicitFeedback(Feedback):
 
-    def __init__(self, item_id: int, contexts: List[Context]):
+    def __init__(self, item_id: int, user_id: int):
         self.item_id = item_id
-        self.contexts = contexts
+        self.user_id = user_id
